1af3101e349ba22e8e46dc8b249b813d30afd933
Patri <pmartinperez@danielcastelao.org>
2016-04-06 09:29:22 +0200

    Inicio

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+  'name':'Chachi tema',
+  'description': 'Un tema muy chachi',
+  'version':'1.0',
+  'author':'Patripon',
+
+  'data': [
+  'views/layout.xml',
+  'views/pages.xml',
+  'views/assets.xml',
+  'views/snippets.xml',
+],
+  'category': 'Theme/Creative',
+  'depends': ['website', 'website_blog', 'sale'],
+}
